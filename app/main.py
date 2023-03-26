dad6a5c7818e0424155bf7c8e1e7a2544bc01e83
caretim <karesin72041@gmail.com>
2023-03-26 23:35:54 +0900

    guide

--- app/main.py
+++ app/main.py
@@ -1,14 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
+
+#
+# 지금까지는 GET방식이기에 URL 및 Request Header로 값이 넘어왔다.
+#  하지만 POST방식에서는 Request Body로 넘어오기에 이전가지의 방식으로는 데이터를 받을 수 없다.
+# Request Body는 Pydantic Model을 이용하여 값을 받을 수 있다.
+# 1. from pydantic import BaseModel를 통해 class구성 
 app = FastAPI()
 
+class Model(BaseModel):
+    name: str
+    phoneNumber :int
 
 @app.get("/")
 async def root():
-    return {"message": "오늘 저녁은 뭐먹지?"}
-
+    return {"message": "루트페이지"}
 
 
 @app.get("/login")
 async def root():
     return {"signup": "회원가입페이지"}
+
+
+@app.get("/cat")
+async def cat():
+    return{'고양이':'야옹'}
+
+
+#post사용 방법 
+@app.post("/send") # app.+메서드 
+def data받음(data:Model):
+    print(data)
+    return '전송완료'
