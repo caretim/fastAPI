f5acaeb4923e921764dcb17beefc544db7d3b9ac
caretim <karesin72041@gmail.com>
2023-04-10 15:48:13 +0900

    crud

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from typing import Union
 from pydantic import BaseModel
 from typing import Optional
-from database import engine
+from database import engine,get_db
 from models import Question,Answer
 from datetime import datetime
 import models
+import requests
+from sqlalchemy.orm import Session # orm 세션 
 
 from database import get_db
 # 경로 동작은 순차적으로 지원됨 동일 경로롤 지나칠때 상위 경로에 지정되어있는주소가 있다면
@@ -19,11 +21,12 @@ from database import get_db
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+db=[]
 
 
-class Item(BaseModel):
+class City(BaseModel):
     name: str
-    price: float
+    timezone:str
     is_offer: Union[bool , None] = None
     #None 명시는 초기값으로 필수로 안받아도 된다는뜻, 
     # 만약 설정을 안한다면 필수로 받아야함
@@ -36,31 +39,47 @@ async def root():
 async def root():
     return {"signup": "회원가입페이지"}
 
+#city 전체 조회
+@app.get("/cities/")
+async def get_items():
+    results=[]
+    for city in db:
+        strs=f"http://worldtimeapi.org/timezone/{city['timezone']}"
+        header = {"User-Agent": "Mozilla/5.0"}
+        r= requests.get(strs,headers=header)
+        print(r)
+        cur_time=r.json()['datetime']
+        results.append({'name':city['name'], 'timezone':city['timezone'] , 'current_time':cur_time})
+    return results
+#id조회 read
+@app.get("/cities/{city_id}")
+async def create_city(city_id:int):
+    city = db[city_id-1]
+    strs="http://worldtimeapi.org/timezone/Asia/Seoul"
+    r= requests.get(strs)
 
-@app.put("/items/{item_id}/") 
-def update_item(item_id:int , item = Item):
-    return{"item_name":item.name, "item_id":item_id}
-## 흠.. 모델메타클래스는 시리얼라이저를 왜 사용이 안되지? 
+    cur_time=r.json()['datetime']
+    return {'name':city['name'],{'current_time'}:cur_time}
+
+#생성
+@app.post("/cities")
+async def create_city(city:City):
+    db.append(city.dict())
+
+    return db[-1]
+
+@app.delete("/cities/{city_id}")
+async def delete_city(city_id:int):
+    db.pop(city_id-1)
+
+    return f"{city_id}번 삭제성공"
 
 
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str,None] = None):
-    return {"item_id": item_id , "q": q} 
-
-
-@app.post('/items/')
-async def create_item(item: Item) -> Item:
-    return item
-
-
+    
 # 함수가 받은(반환도 하는) 값은 문자열 "3"이 아니라 파이썬 int 형인 3입니다.
 
 # 즉, 타입 선언을 하면 FastAPI는 자동으로 요청을 "파싱"합니다.
 
-@app.post('/test')
-def test(item:Item)-> Item:
-    q = Question(subject='FastAPI 모델 질문입니다.', content='id는 자동으로 생성되나요?', create_date=datetime.now())
-    get_db.add(q)
-    get_db.commit()
-    return "저장완료"
+
+
+
